a9376c9cc6c2b01f7ac1a1c25f5203fa486df8cb
Smith Tools <noreply@smith-tools.io>
2025-11-21 16:56:18 +0100

    Bump CLI version to v1.0.7

--- Sources/smith-validation/main.swift
+++ Sources/smith-validation/main.swift
@@ -347,7 +347,7 @@ struct smith_validation {
     }
 
     private static func versionString() -> String {
-        return "v1.0.4"
+        return "v1.0.7"
     }
 
     /// Attempt to load PKL configuration if the file exists.
